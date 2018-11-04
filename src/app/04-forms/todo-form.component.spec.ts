bfa7f8e5deced85d1767453ff7f4699a4e433fd5
w1lltj <cycorn8@gmail.com>
2018-11-05 00:27:23 +0800

    testing form

---
+++ src/app/04-forms/todo-form.component.spec.ts
@@ -0,0 +1,21 @@
+import { TodoFormComponent } from './todo-form.component';
+import { FormBuilder } from '@angular/forms';
+
+describe('TodoFormComponent', () => {
+  let component: TodoFormComponent;
+
+  beforeEach(() => {
+    component = new TodoFormComponent(new FormBuilder());
+  });
+
+  it('should create form with 2 controls', () => {
+    expect(component.form.contains('name')).toBeTruthy();
+    expect(component.form.contains('email')).toBeTruthy();
+  });
+
+  it('should make name required', () => {
+    const control = component.form.get('name');
+    control.setValue('');
+    expect(control.valid).toBeFalsy();
+  });
+});
