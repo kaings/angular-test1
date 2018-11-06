e96d42819ee8b87e4b08b3457bbf0db64422899b
w1lltj <cycorn8@gmail.com>
2018-11-06 16:18:31 +0800

    integration test: test property and class binding

--- src/app/1-voter/voter.component.spec.ts
+++ src/app/1-voter/voter.component.spec.ts
@@ -1,5 +1,6 @@
 import { VoterComponent } from './voter.component';
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
 
 describe('VoterComponent', () => {
   let component: VoterComponent;
@@ -15,6 +16,26 @@ describe('VoterComponent', () => {
     component = fixture.componentInstance;
   });
 
-  it('', () => {
+  it('Should render totalVotes', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+
+    fixture.detectChanges();
+
+    const de = fixture.debugElement.query(By.css('.vote-count'));
+    const el: HTMLElement = de.nativeElement;
+
+    expect(el.innerText).toContain('21');
+  });
+
+  it('Should highlight the upvote button if it is upVoted', () => {
+    component.myVote = 1;
+
+    fixture.detectChanges();
+
+    const de = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+    // or can just selecting '.highlighted' directly which will return true or false
+
+    expect(de.classes['highlighted']).toBeTruthy();
   });
 });
