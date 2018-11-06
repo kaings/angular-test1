5dd0323741e7311964ccdd24f3785db20de713c9
w1lltj <cycorn8@gmail.com>
2018-11-06 12:57:08 +0800

    integration-test: setup code (manual)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
+import {VoterComponent} from './1-voter/voter.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoterComponent
   ],
   imports: [
     BrowserModule,
