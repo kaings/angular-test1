3ecafec2efab3088c528fa617f996205e25f6c20
w1lltj <cycorn8@gmail.com>
2018-11-07 00:26:13 +0800

    integration test: getting dependencies

--- src/app/2-todos/todos.component.spec.ts
+++ src/app/2-todos/todos.component.spec.ts
@@ -6,6 +6,7 @@ import { DebugElement } from '@angular/core';
 import { TodosComponent } from './todos.component';
 import {TodoService} from './todo.service';
 import {HttpClientModule} from '@angular/common/http';
+import {from} from 'rxjs';
 
 // NOTE: I've deliberately excluded this suite from running
 // because the test will fail. This is because we have not
@@ -30,10 +31,20 @@ describe('TodosComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TodosComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Should load todos from the server', () => {
+    const service = TestBed.get(TodoService);
+
+    spyOn(service, 'getTodos').and.returnValue(from([ [1, 2, 3, 4, 5, 6, 7, 8] ]));
+
+    // different from unit-test, in integration test, when test begins, ngOnInit will be called.
+    // This means if detectChanges() is called in beforeEach(), it will trigger getTodos which will
+    // return todos=0 instead of fakeCall returning array. Therefore, detectChanges must be
+    // put after the fakeCall/ returnValue
+    fixture.detectChanges();
+
+    expect(component.todos.length).toBe(8);
   });
 });
